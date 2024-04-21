ba3b6d4e2b544cd0910ef38a54cec1ee160c7df2
BogBogdan <bogdantomicbttomic@gmail.com>
2024-04-21 19:04:28 +0200

    sad je stvarno kraj

--- cypress/e2e/krediti/OdobriOdbiKredit.cy.ts
+++ cypress/e2e/krediti/OdobriOdbiKredit.cy.ts
@@ -1,24 +1,92 @@
-import { loginAdmin, logout } from "../util/util"
+import { loginAdmin, loginKorisnik, logout } from "../util/util"
 
 describe('Kredit spec', () => {
   beforeEach(() => {
-    loginAdmin(cy);
+    //loginAdmin(cy);
   })
   after(() => {
     //logout(cy)
   })
   it('Odobri prvi kredit', () => {
     
+    loginKorisnik(cy);
+    cy.wait(200)
+    cy.visit('http://localhost:3000/listaKredita')
+    cy.wait(200)
+    cy.get("#TraziKredit").click()
+
+    cy.get('#vrstaKredita').click()
+    // Sačekaj da se prikažu opcije
+    cy.wait(1000)
+    // Izaberi opciju "Gotovinski"
+    cy.get('[role="option"]').contains('Gotovinski').click()
+    // Popuni ostala polja forme
+    cy.get('input[name="amount"]').type('10000')
+    cy.get('input[name="loanPurpose"]').type('Kupovina automobila')
+    cy.get('input[name="salary"]').type('50000')
+    cy.get('input[name="permanentEmployee"]').check()
+    cy.get('input[name="currentEmploymentPeriod"]').type('5 godina')
+    cy.get('input[name="loanTerm"]').type('60')
+    cy.get('#bankAccountNumber').click()
+    cy.wait(1000)
+    cy.get('[role="option"]').contains('444000000910000033').click()
+    cy.get('input[name="branchOffice"]').type('Beograd')
+    cy.wait(200)
+    cy.get("#kwiknimeUwU").click();
+    
+    cy.wait(500)
+
+    cy.wait(200)
+    logout(cy)
+    cy.wait(200)
+    loginAdmin(cy);
+    cy.wait(200)
+
     cy.visit('http://localhost:3000/listaKredita')
     cy.wait(200) // Sačekajte 200 milisekundi (prilagodite vreme čekanja po potrebi)
     cy.contains('ODOBRI').eq(0).click()
 
+    logout(cy)
   })
 
   it('Odbij prvi kredit', () => {
-   
+    loginKorisnik(cy);
+    cy.wait(200)
+    cy.visit('http://localhost:3000/listaKredita')
+    cy.wait(200)
+    cy.get("#TraziKredit").click()
+
+    cy.get('#vrstaKredita').click()
+    // Sačekaj da se prikažu opcije
+    cy.wait(1000)
+    // Izaberi opciju "Gotovinski"
+    cy.get('[role="option"]').contains('Gotovinski').click()
+    // Popuni ostala polja forme
+    cy.get('input[name="amount"]').type('10000')
+    cy.get('input[name="loanPurpose"]').type('Kupovina automobila')
+    cy.get('input[name="salary"]').type('50000')
+    cy.get('input[name="permanentEmployee"]').check()
+    cy.get('input[name="currentEmploymentPeriod"]').type('5 godina')
+    cy.get('input[name="loanTerm"]').type('60')
+    cy.get('#bankAccountNumber').click()
+    cy.wait(1000)
+    cy.get('[role="option"]').contains('444000000910000033').click()
+    cy.get('input[name="branchOffice"]').type('Beograd')
+    cy.wait(200)
+    cy.get("#kwiknimeUwU").click();
+    
+    cy.wait(500)
+
+    cy.wait(200)
+    logout(cy)
+    cy.wait(200)
+    loginAdmin(cy);
+    cy.wait(200)
+
     cy.visit('http://localhost:3000/listaKredita')
     cy.wait(200) // Sačekajte 200 milisekundi (prilagodite vreme čekanja po potrebi)
     cy.contains('ODBIJ').eq(0).click()
+
+    logout(cy)
   })
 })
