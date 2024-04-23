fa85c599279409b86fcaaae52657eb8ae0ad2d7e
BogBogdan <bogdantomicbttomic@gmail.com>
2024-04-23 19:44:44 +0200

    ssve napisano

--- cypress/e2e/kartice/kartice.cy.ts
+++ cypress/e2e/kartice/kartice.cy.ts
@@ -1,8 +1,8 @@
 import { loginAdmin, loginKorisnik2, logout } from "../util/util"
 
 describe('Kartice spec', () => {
-    it('Rad sa karticama', () => {
-
+    beforeEach(() => {
+        
         //Dodaj kartice
         loginAdmin(cy);
         cy.wait(200);
@@ -42,30 +42,55 @@ describe('Kartice spec', () => {
                     cy.get("#dodajKarticuDugme").click();
                     cy.get("#brojRacunaInputt").type(first18Chars);
                     cy.get("#buttonKreiraj").click();
-                    cy.wait(10000);
+                    cy.wait(1000);
                     logout(cy);
                     cy.wait(2000);
-                    //Deo za dodavanje
-
-                    loginKorisnik2(cy);
-                    cy.visit('http://localhost:3000/kartice');
-                    cy.wait(1000);
-
-                    cy.get('table')
-                        .find('tbody')
-                        .children('tr')
-                        .then(rows => {
-                            let rowCount;
-                            if (rows.length === 1 && rows.eq(0).find('td').length === 1) {
-                                rowCount = 0;
-                            } else {
-                                rowCount = rows.length;
-                            }
-
-                            cy.wrap(rowCount).should('equal', initialRowCountUser + 1);
-
-                        });
+               
                 });
-        
+      })
+    it('Rad sa karticama blokiraj korisnik', () => {
+
+             //Deo za dodavanje
+
+             loginKorisnik2(cy);
+             cy.visit('http://localhost:3000/kartice');
+             cy.wait(1000);
+
+             cy.get('tbody > tr').first().click();
+             cy.wait(2000);
+             cy.contains('button', 'Blokiraj').click();
+             cy.wait(1000);
     })
+
+    it('Rad sa karticama blokiraj admin', () => {
+
+        //Deo za dodavanje
+
+        loginAdmin(cy);
+        cy.visit('http://localhost:3000/kartice');
+        cy.wait(1000);
+
+        cy.get('tbody > tr').first().click();
+        cy.wait(2000);
+        cy.contains('button', 'Blokiraj').click();
+        cy.wait(1000);
+})
+
+it('Rad sa karticama aktiviraj deaktiviraj', () => {
+
+    //Deo za dodavanje
+
+    loginAdmin(cy);
+    cy.visit('http://localhost:3000/kartice');
+    cy.wait(1000);
+
+    cy.get('tbody > tr').first().click();
+    cy.wait(2000);
+    cy.contains('button', 'Deaktiviraj').click();
+    cy.wait(1000);
+    cy.contains('button', 'Ok').click();
+    cy.wait(1000);
+    cy.contains('button', 'Aktiviraj').click();
+    cy.wait(1000);
+})
 })
