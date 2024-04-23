10ffa53be75e887e5635c837b64dfd55e3d1219b
BogBogdan <bogdantomicbttomic@gmail.com>
2024-04-23 18:43:28 +0200

    proba

--- cypress/e2e/kartice/kartice.cy.ts
+++ cypress/e2e/kartice/kartice.cy.ts
@@ -7,11 +7,11 @@ describe('Kartice spec', () => {
         loginAdmin(cy);
         cy.wait(200);
         cy.get('tbody > tr').first().click();
-        cy.wait(200);
+        cy.wait(2000);
 
         cy.get('#RacuniTabela').should('exist').then(() => {
             cy.log('POSTOJI');
-            cy.get('#RacuniTabela tbody > tr:first-child > td:first-child').invoke('text').then((text) => {
+            cy.get('#RacuniTabela tr:first-child td:first-child').invoke('text').then((text) => {
                 // text sadrži vrednost iz prve kolone prvog reda
                 // ovde možeš izvršiti dalje provere ili akcije sa ovom vrednošću
                 cy.log('Vrednost iz prve kolone prvog reda:', text);
