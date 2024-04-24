6c49950d78c11d994a3c4f2bf5d718bf6f24e9ca
BogBogdan <bogdantomicbttomic@gmail.com>
2024-04-24 17:53:27 +0200

    errori

--- cypress/e2e/racuni/racuni.cy.ts
+++ cypress/e2e/racuni/racuni.cy.ts
@@ -69,4 +69,55 @@ describe('Provera racuna', () => {
           cy.contains('button', "Kreiraj").click();
   
       })
+
+
+      it('pravi racun Devizni bez checkboxova', () => {
+
+        cy.wait(2000)
+        cy.get('table tbody tr:first-child td:first-child').click();
+    
+    
+            cy.wait(2000)
+            cy.contains('button', "Dodaj racun").click();
+    
+    
+            cy.wait(2000)
+    
+            cy.get('#mui-component-select-Tip').click();
+    
+            cy.get('[role="option"]').contains('Devizni').click()
+            
+            cy.contains('button', "Pretraga Korisnika").click();
+    
+            cy.wait(2000)
+            
+            cy.contains('button', "Kreiraj").click();
+    
+        })
+
+
+        it('pravi racun Tekuci bez vrste', () => {
+
+          cy.wait(2000)
+          cy.get('table tbody tr:first-child td:first-child').click();
+      
+      
+              cy.wait(2000)
+              cy.contains('button', "Dodaj racun").click();
+      
+      
+              cy.wait(2000)
+      
+              cy.get('#mui-component-select-Tip').click();
+      
+              cy.get('[role="option"]').contains('Tekuci').click()
+      
+              
+              cy.contains('button', "Pretraga Korisnika").click();
+      
+              cy.wait(2000)
+              
+              cy.contains('button', "Kreiraj").click();
+      
+          })
 })
