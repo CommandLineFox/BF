28994e2c90060efd4eaa7b56e238d9852c5ce865
BogBogdan <bogdantomicbttomic@gmail.com>
2024-04-24 15:02:47 +0200

    ss

--- cypress/e2e/korisnici/korisnici.cy.ts
+++ cypress/e2e/korisnici/korisnici.cy.ts
@@ -24,9 +24,10 @@ describe('Provera kursa spec', () => {
     cy.get('input[name="prezime"]').type(prezime);
     cy.get('input[name="jmbg"]').type(jmbg);
     cy.get('input[name="date"]').type(datumRodjenja);
-    cy.contains('Pol').click({force: true});
-    cy.wait(1000);
-    cy.contains('Musko').click({force: true});
+    cy.get('select[name="Pol"]').scrollIntoView();
+
+    // Izbor opcije "Musko"
+    cy.get('select[name="Pol"]').select('Musko');
     
     cy.get('input[name="adresa"]').type(adresa);
     cy.get('input[name="email"]').type(email);
