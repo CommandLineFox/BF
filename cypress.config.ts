dc5f5c58a510cb951ba88c6dd88687aa062368a7
Aleksa Buncic <alextromb1@gmail.com>
2024-04-26 00:39:50 +0200

    a

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     experimentalRunAllSpecs: true,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
